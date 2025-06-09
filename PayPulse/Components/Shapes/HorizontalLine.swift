e49d21787070c0e13d763fa726aec0470e5568bf
azfar-imtiaz <azfar.imtiaz@live.com>
2025-06-10 00:48:29 +0200

    Create initial login view

---
+++ PayPulse/Components/Shapes/HorizontalLine.swift
@@ -0,0 +1,19 @@
+//
+//  HorizontalLine.swift
+//  PayPulse
+//
+//  Created by Azfar Imtiaz on 2025-06-10.
+//
+
+import SwiftUI
+
+struct HorizontalLine: View {
+    var body: some View {
+        Rectangle()
+            .frame(height: 1)
+    }
+}
+
+#Preview {
+    HorizontalLine()
+}
