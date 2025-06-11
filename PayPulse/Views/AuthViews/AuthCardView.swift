c6d161ecaa82cb63c0c5631199c3a1f762a4351e
azfar-imtiaz <azfar.imtiaz@live.com>
2025-06-11 21:45:25 +0200

    Remove rounded rectangle card, fix for dark mode

--- PayPulse/Views/AuthViews/AuthCardView.swift
+++ PayPulse/Views/AuthViews/AuthCardView.swift
@@ -10,16 +10,10 @@ import SwiftUI
 struct AuthCardView<Content: View>: View {
     let content: Content
     
-    var body: some View {
-        
-        RoundedRectangle(cornerRadius: 10)
-            .frame(height: UIScreen.main.bounds.height * 0.65)
+    var body: some View {        
+        content
             .padding(25)
-            .foregroundStyle(.white)
-            .shadow(color: Color.black.opacity(0.15), radius: 15, x: 0, y: 10)
-            .overlay {
-                content
-            }
+            .frame(maxWidth: .infinity)
     }
 }
 
